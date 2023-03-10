59f8f6154bdf8c65b8522a85e1d42112c6fc27b7
Marc-Andre Bourassa <boumar04@gmail.com>
2023-03-09 19:08:23 -0500

    dynamic data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Meta, Title } from '@angular/platform-browser'
+import { ActivatedRoute, Router } from '@angular/router'
+import { of } from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['../demo-styling.css']
 })
-export class AppComponent {
-  title = 'angular-quickstart';
+export class AppComponent implements OnInit {
+
+  data: Data = {
+    title: "My prerendered site",
+    description: "Awesome prerendered site",
+  }
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private meta: Meta,
+    private title: Title
+  ) { }
+
+  ngOnInit() {
+    this.getData()
+      .subscribe(data => {
+        this.setMetaData(data)
+      })
+  }
+
+  getData() {
+    return of(this.data)
+  }
+
+  setMetaData(data: Data) {
+    this.title.setTitle(data.title)
+    this.meta.updateTag({ 'name': 'description', 'content': data.description })
+    this.meta.updateTag({ 'name': 'twitter:card', 'content': 'summary_large_image' })
+    this.meta.updateTag({ 'name': 'twitter:title', 'content': data.title })
+    this.meta.updateTag({ 'name': 'twitter:text:title', 'content': data.title })
+    this.meta.updateTag({ 'name': 'twitter:description', 'content': data.description })
+    this.meta.updateTag({ 'name': 'twitter:image:alt', 'content': data.title })
+    this.meta.updateTag({ 'property': 'og:title', 'content': data.title })
+    this.meta.updateTag({ 'property': 'og:image:alt', 'content': data.title })
+    this.meta.updateTag({ 'property': 'og:description', 'content': data.description })
+  }
 }
+
+export interface Data { title: string; description: string }
